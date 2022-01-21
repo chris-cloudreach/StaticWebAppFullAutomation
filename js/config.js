a62d4cf532893e467a6b07e7732cefee9afaef00
Christopher <christopher.ogbunuzor@cloudreach.com>
2022-01-21 19:55:25 +0000

    DESTROY - Destroy resources after Challenge

--- js/config.js
+++ js/config.js
@@ -1,13 +1,13 @@
 window._config = {
     cognito: {
-        userPoolId : "eu-west-2_glUh7ssdM",
+        xyz,
         
-        userPoolClientId : "24iu93gbdifn6l9knfgrhfaodb",
+        xxx,
         
         region: 'us-east-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl : "https://fabzbqbt8c.execute-api.eu-west-2.amazonaws.com/DEV"
+        zzz
         
     }
 };
